b85e02e87c667bc94f196d14364cfc6fbb40702e
bismarck <bismarckdados@gmail.com>
2023-10-03 09:48:46 -0300

    init project

---
+++ src/test/java/com/pessoal/esebo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pessoal.esebo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
